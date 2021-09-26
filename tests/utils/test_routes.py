5e3691de47eba59b69854e758c5cb7eb5033cc4c
Brad Erickson <bbwe24@gmail.com>
2021-09-26 13:25:00 -0400

    update

---
+++ tests/utils/test_routes.py
@@ -0,0 +1,15 @@
+from src.utils.routes import navbar_toggle
+
+
+def test_navbar_toggle():
+
+    clicks = 0
+    is_open = True
+    expected = is_open
+    output = navbar_toggle(clicks, is_open)
+    assert output == expected
+
+    clicks += 1
+    expected = not is_open
+    output = navbar_toggle(clicks, is_open)
+    assert output == expected
