9262ad0c56afe2360cdc53c2a8f521b3cbc849bb
Sharath Kumar <sharu.bhavimane@gmail.com>
2021-09-29 02:28:30 -0400

    added Dockerfile and modified index.py to be compatible with docker run environment

--- src/plotting/index.py
+++ src/plotting/index.py
@@ -1,5 +1,6 @@
 # package imports
 from plotting.app import app
+import argparse
 
 # local imports
 from environment.settings import APP_HOST, APP_PORT, APP_DEBUG, DEV_TOOLS_PROPS_CHECK
@@ -15,8 +16,12 @@ server = app.server
 
 # site endpoint
 if __name__ == '__main__':
+    my_parser = argparse.ArgumentParser(description="CmyPlot")
+    my_parser.add_argument('--host', action='store', default='127.0.0.1', type=str, metavar='host address',
+                           help="Host address where the website has to be deployed")
+    args = my_parser.parse_args()
     app.run_server(
-        host=APP_HOST,
+        host=args.host,
         port=APP_PORT,
         debug=APP_DEBUG,
         dev_tools_props_check=DEV_TOOLS_PROPS_CHECK
