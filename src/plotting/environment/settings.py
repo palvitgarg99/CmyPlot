c2995da90a223e67784f8a465752a32b025796e6
palvitgarg99 <palvitgarg99@gmail.com>
2021-10-31 03:27:20 -0400

    Add debug flag

--- src/plotting/environment/settings.py
+++ src/plotting/environment/settings.py
@@ -12,3 +12,5 @@ APP_HOST = os.environ.get("HOST")
 APP_PORT = int(os.environ.get("PORT"))
 APP_DEBUG = bool(os.environ.get("DEBUG"))
 DEV_TOOLS_PROPS_CHECK = bool(os.environ.get("DEV_TOOLS_PROPS_CHECK"))
+
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
